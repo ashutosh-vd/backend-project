ef02c8f324536c68a461eb19d569069250447275
ashutosh-vd <ashutoshsensei09@gmail.com>
2025-02-04 23:29:53 +0530

    new top 😎

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import fs, { read } from "node:fs"
+
 import express from "express";
 const app = express();
+
 import path from 'path'
 import { fileURLToPath } from "url";
+import { setFips } from "node:crypto";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,7 +19,25 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')  						// to use ejs
 
 app.get('/', (req, res) => {
-	res.render('index')
+	fs.readdir('./files', (err, files) => {
+		res.render('index', {files : files});
+	})
+})
+
+app.get('/profile/:username', (req, res) => {
+	// res.render('index')
+	const name = req.params.username
+	res.send(`Kya chal Raha Hai ${name} ?`)
+})
+
+app.post('/create' , (req, res) => {
+	// console.log(req.body)
+	let filename = `${req.body.title.split(' ').join('_')}.txt`
+	fs.writeFile(`./files/${filename}`, req.body.details, (err) => {
+		if(err) {
+			console.log("write Error");
+		}
+	})
 })
 
 app.listen(3000, () => {
